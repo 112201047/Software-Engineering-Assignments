5b68d31876fda40a97e6968258d5d605b98559b4
Nikhil S Thomas <nikhil.s.thomas5@gmail.com>
2025-09-08 23:23:24 +0530

    WPF App Sample Intial Commit

---
+++ WpfApp-Sample/Networking/Communicator.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Networking;
+
+public class Communicator
+{
+    FileSystemWatcher _watcher;
+    IFileChangedHandler? _handler;
+
+    public Communicator(string path)
+    {
+        _watcher = new FileSystemWatcher(path);
+        _watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.CreationTime;
+        _watcher.Filter = "*.*";
+        _watcher.Created += OnChanged; // Ok to have created and changed both point to OnChanged
+        _watcher.Changed += OnChanged;
+        _watcher.EnableRaisingEvents = true;
+    }
+
+    public void Subscribe(IFileChangedHandler handler)
+    {
+        _handler = handler;
+    }
+
+    private void OnChanged(object sender, FileSystemEventArgs e)
+    {
+        if (_handler != null)
+        {
+            if (e.FullPath.EndsWith(".txt") || e.FullPath.EndsWith(".png"))
+            {
+                _handler.OnFileChanged(e.FullPath);
+            }
+        }
+    }
+}
---
+++ WpfApp-Sample/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Networking;
+using System;
+using System.Windows;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, IFileChangedHandler
+{
+    Communicator _communicator;
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        _communicator = new Communicator(path);
+        _communicator.Subscribe(this);
+    }
+
+    public void OnFileChanged(string filePath)
+    {
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            fileUpdatedTextBlock.Text = $"File updated: {filePath}";
+
+            if (filePath.EndsWith(".png"))
+            {
+                try
+                {
+                    var bitmap = new System.Windows.Media.Imaging.BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(filePath);
+                    bitmap.CacheOption = System.Windows.Media.Imaging.BitmapCacheOption.OnLoad;
+                    bitmap.EndInit();
+                    fileUpdatedImage.Source = bitmap;
+                }
+                catch (Exception ex)
+                {
+                    fileUpdatedTextBlock.Text += $"\nError loading image: {ex.Message}";
+                }
+            }
+            else
+            {
+                fileUpdatedImage.Source = null;
+            }
+        });
+    }
+}
